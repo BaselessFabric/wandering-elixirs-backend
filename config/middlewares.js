c5a6660987f4a364293f5f297eb41b21536ec432
BaselessFabric <alexanderwalls92@gmail.com>
2023-04-03 21:30:23 +0100

    fixed API key issue, added cloudinary plugin

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,23 @@
 module.exports = [
   "strapi::errors",
-  "strapi::security",
+  module.exports = [
+    // ...
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+            'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+            upgradeInsecureRequests: null,
+          },
+        },
+      },
+    },
+    // ...
+  ];,
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
