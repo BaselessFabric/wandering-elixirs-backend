09af7e44799115dbb54b4a349a2054d5b9f4a023
BaselessFabric <alexanderwalls92@gmail.com>
2023-04-05 19:51:42 +0100

    Correctly added routes

--- src/api/item/controllers/item.js
+++ src/api/item/controllers/item.js
@@ -4,58 +4,74 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY); // Import the S
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::item.item", ({ strapi }) => ({
-  // Define a custom action to handle the webhook request from Stripe
-  async handleWebhook(ctx) {
-    const { body, headers } = ctx.request;
-    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+module.exports = createCoreController(
+  "api::item.item",
+  ({ strapi }) => ({
+    // Define a custom action to handle the webhook request from Stripe
+    async handleWebhook(ctx) {
+      const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+      const sig = ctx.request.headers["stripe-signature"];
 
-    try {
-      const event = stripe.webhooks.constructEvent(
-        body,
-        headers["stripe-signature"],
-        webhookSecret
-      );
-
-      if (event.type === "checkout.session.completed") {
-        const checkoutSession = await stripe.checkout.sessions.retrieve(
-          event.data.object.id
+      try {
+        const event = stripe.webhooks.constructEvent(
+          ctx.request.rawBody,
+          sig,
+          webhookSecret
         );
 
-        // Get the line items for the checkout session
-        const lineItems = await stripe.checkout.sessions.listLineItems(
-          checkoutSession.id
-        );
-
-        // Loop through the line items in the checkout session
-        for (const lineItem of lineItems.data) {
-          const { price, quantity } = lineItem;
-          const productId = price.product;
-
-          // Retrieve the item from your Strapi database using the Strapi SDK
-          const product = await strapi.services.item.findOne({
-            id: productId,
-          });
-
-          // Calculate the new stock level
-          const newStock = product.stockLevel - quantity;
-
-          // Update the stock level in your Strapi database using the Strapi SDK
-          await strapi.services.item.update(
-            { id: productId },
-            { stockLevel: newStock }
+        if (event.type === "checkout.session.completed") {
+          const checkoutSession = await stripe.checkout.sessions.retrieve(
+            event.data.object.id
           );
+
+          // Get the line items for the checkout session
+          const lineItems = await stripe.checkout.sessions.listLineItems(
+            checkoutSession.id
+          );
+
+          // Loop through the line items in the checkout session
+          for (const lineItem of lineItems.data) {
+            const { price, quantity } = lineItem;
+            const productId = price.product;
+
+            // Retrieve the item from your Strapi database using the Strapi SDK
+            const product = await strapi.services.item.findOne({
+              id: productId,
+            });
+
+            // Calculate the new stock level
+            const newStock = product.stockLevel - quantity;
+
+            // Update the stock level in your Strapi database using the Strapi SDK
+            await strapi.services.item.update(
+              { id: productId },
+              { stockLevel: newStock }
+            );
+          }
+
+          // Send a success response to Stripe
+          ctx.send({ received: true });
         }
-
-        // Send a success response to Stripe
-        ctx.send({ received: true });
+      } catch (err) {
+        // Handle any errors that occur during the update process
+        console.log(err);
+        ctx.send({ received: false, error: err });
       }
-    } catch (err) {
-      // Handle any errors that occur during the update process
-      console.log(err);
-      ctx.send({ received: false, error: err });
-    }
-  },
+    },
 
-  // Define your other controller actions here, such as index, create, update, etc.
-}));
+    // Define your other controller actions here, such as index, create, update, etc.
+  }),
+  {
+    // Define the Koa middleware to parse the request body
+    middleware: [
+      async function parseBody(ctx, next) {
+        ctx.request.rawBody = await new Promise((resolve) => {
+          let data = "";
+          ctx.req.on("data", (chunk) => (data += chunk));
+          ctx.req.on("end", () => resolve(data));
+        });
+        await next();
+      },
+    ],
+  }
+);
