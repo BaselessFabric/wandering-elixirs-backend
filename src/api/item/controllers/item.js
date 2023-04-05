75b850137dd0ec84657103c0a6bf2dcb05fce8a6
BaselessFabric <alexanderwalls92@gmail.com>
2023-04-05 15:16:05 +0100

    Added webhook to update product stock

--- src/api/item/controllers/item.js
+++ src/api/item/controllers/item.js
@@ -10,6 +10,8 @@ module.exports = createCoreController("api::item.item", {
     try {
       const { data } = ctx.request.body;
 
+      console.log(data);
+
       // Loop through the line items in the Stripe webhook payload
       for (const item of data.object.lines.data) {
         const { id, quantity } = item; // Extract the product ID and quantity
@@ -17,8 +19,13 @@ module.exports = createCoreController("api::item.item", {
         // Retrieve the item from your Strapi database using the Strapi SDK
         const product = await strapi.services.item.findOne({ id });
 
+        console.log(product);
+
         // Calculate the new stock level
         const newStock = product.stockLevel - quantity;
+
+        console.log(stockLevel);
+        console.log(newStock);
 
         // Update the stock level in your Strapi database using the Strapi SDK
         await strapi.services.item.update({ id }, { stockLevel: newStock });
