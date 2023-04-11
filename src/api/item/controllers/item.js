be80b0da3f961597b3991592d5db4d161b797a0e
BaselessFabric <alexanderwalls92@gmail.com>
2023-04-11 17:28:04 +0100

    lower rez photos, remove extra icons on navbar, put correct success and fail urls

--- src/api/item/controllers/item.js
+++ src/api/item/controllers/item.js
@@ -9,30 +9,6 @@ const { createCoreController } = require("@strapi/strapi").factories;
 module.exports = createCoreController("api::item.item", ({ strapi }) => ({
   // Define a custom action to handle the webhook request from Stripe
   async handleWebhook(ctx) {
-    // console.log(stripe);
-
-    // const sig = ctx.request.headers["stripe-signature"];
-    // console.log("sig: ", sig);
-    // console.log("data :", ctx.request.data);
-    // console.log("body :", ctx.request.body.id);
-    // console.log("stringify: ", JSON.stringify(ctx.request.body));
-    // console.log("rawBody :", ctx.request.rawBody);
-    // console.log("request :", ctx.request);
-
-    // const sessionId = ctx.request.body.data.object.id;
-    // console.log("session: ", sessionId);
-
-    // try {
-
-    //   const event = stripe.webhooks.constructEvent(
-    //     ctx.request.body,
-    //     sig,
-    //     webhookSecret
-    //   );
-
-    //   console.log("event: ", event);
-    //   console.log("Error:", err.message);
-
     try {
       if (ctx.request.body.type === "checkout.session.completed") {
         const checkoutSession = await ctx.request.body.data.object.id;
@@ -42,34 +18,26 @@ module.exports = createCoreController("api::item.item", ({ strapi }) => ({
           checkoutSession
         );
 
-        console.log("lineitems: ", lineItems.data);
-        // console.log("data.price informatgion: ", lineItems.data.price);
-        console.log("lineItems length:", lineItems.data.length);
-
         // Loop through the line items in the checkout session
         for (const lineItem of lineItems.data) {
           const { price, quantity, description, reference_id, name } = lineItem;
-          //   const custom_product_id = price.description.split(":")[1];
-          const custom_product_id = reference_id;
+          const splitArray = lineItem.description.split(":");
+          const custom_product_id = parseInt(splitArray[1]);
 
-          console.log("customproductID: ", custom_product_id);
-          console.log("price: ", price);
-          console.log("quantity: ", quantity);
-
-          // Retrieve the item from your Strapi database using the Strapi SDK
-          const product = await strapi.services.item.findOne({
-            id: custom_product_id,
-          });
-          //   console.log("product: ", product);
+          let product;
+          product = await strapi
+            .service("api::item.item")
+            .findOne(custom_product_id);
 
           // Calculate the new stock level
           const newStock = product.stockLevel - quantity;
 
           // Update the stock level in your Strapi database using the Strapi SDK
-          await strapi.services.item.update(
-            { id: custom_product_id },
-            { stockLevel: newStock }
-          );
+          await strapi.service("api::item.item").update(custom_product_id, {
+            data: {
+              stockLevel: newStock,
+            },
+          });
         }
 
         // Send a success response to Stripe
--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -13,25 +13,17 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
     try {
       // retrieve item information
 
-      console.log(products);
-
       const lineItems = await Promise.all(
         products.map(async (product) => {
           const item = await strapi
             .service("api::item.item")
             .findOne(product.id);
 
-          console.log("item: ", item);
-
           return {
             price_data: {
               currency: "gbp",
               product_data: {
                 name: `${item.name} ID:${product.id}`,
-                description: product.id,
-                metadata: {
-                  custom_product_id: product.id,
-                },
               },
               unit_amount: item.price * 100,
             },
@@ -40,17 +32,14 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
         })
       );
 
-      console.log(lineItems);
-      console.log("productData: ", lineItems.product_data);
-
       // create a stripe session
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
 
         customer_email: email,
         mode: "payment",
-        success_url: "http://localhost:3000/checkout/success",
-        cancel_url: "http://localhost:3000",
+        success_url: "https://wanderingelixirs.com/checkout/success",
+        cancel_url: "https://wanderingelixirs.com",
         line_items: lineItems,
       });
 
